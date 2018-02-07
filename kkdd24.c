ea16fb08652e0196248358ae9add506f362fa598
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-07 15:12:30 +0530

    Create kkdd24.c

---
+++ kkdd24.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int array[MAXSIZE];
+    int i, j, n, t;
+    printf("Enter the value of num \n");
+    scanf("%d", &n);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Input array is \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    /*   Bubble sorting begins */
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < (n - i - 1); j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                t = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t;
+            }
+        }
+    }
+    printf("Sorted array is...\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+}
