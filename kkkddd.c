272b4c675a8431eb50a93ae0e868e84a70f5ab0a
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-07 14:24:47 +0530

    Update and rename kkddkkdd.c to kkkddd.c

--- kkkddd.c
+++ kkkddd.c
@@ -6,18 +6,18 @@ int main()
 {
 int m= 3
 int array [m][m];
-    int array[3], size, i, largest;
+    int array[3], size, i, lowest;
     printf("\n Enter the size of the array: ");
     scanf("%d", &size);
     printf("\n Enter %d elements of  the array: ", size);
     for (i = 0; i < m; i++)
         scanf("%d", &array[i]);
-    largest = array[0];
+    lowest = array[0];
     for (i = 1; i < m; i++)
     {
-        if (largest < array[i])
-            largest = array[i];
+        if (lowest< array[i])
+            lowest = array[i];
     }
-    printf("\n largest element present in the given array is : %d", largest);
+    printf("\n lowest element present in the given array is : %d", lowest);
     return 0;
 }
