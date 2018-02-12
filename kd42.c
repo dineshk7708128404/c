27278c4d550d3ca0ddc3e31c419a40cb6913195f
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-12 13:08:59 +0530

    Update kd42.c

--- kd42.c
+++ kd42.c
@@ -6,7 +6,7 @@ void main()
    char str[100];
    int i = 0, l = 0, f = 1;
    clrscr();
-   puts("Enter any string\n");
+   puts("hello world\n");
    gets(str);
    for(i = 0; str[i] !='\0'; i++)
    {
