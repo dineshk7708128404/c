6e450848656c9994c0a50f74132f9056cf62c52c
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-21 14:11:52 +0530

    Create kd49.c

---
+++ kd49.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+int main() {
+   int a=5, b=5, c;
+ 
+   printf("\nEnter two no:a,b ");
+   scanf("%d %d", &a, &b);
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", c);
+ 
+   return(0);
+}
