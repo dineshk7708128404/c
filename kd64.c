e40a7956a8ef5e613b4a8cfe640f93ddd005deff
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-28 14:13:51 +0530

    Create kd64.c

---
+++ kd64.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+ 
+int main()
+{
+   int a, b, c;
+ 
+   printf("Enter two numbers to add\n");
+   scanf("%d%d", &a, &b);
+ 
+   c = a + b;
+ 
+   printf("Sum of the numbers = %d\n", c);
+   {  scanf("%d", &c);
+
+
+ if(c % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
+}
